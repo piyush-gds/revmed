1312d33a9cce52cd8e886cc5954c1ff962646902
IN010166067_EYGS <Piyush.Jain17@gds.ey.com>
2026-01-12 21:43:52 +0530

    a

--- blocks/helpful-resources/helpful-resources.js
+++ blocks/helpful-resources/helpful-resources.js
@@ -1,3 +1,3 @@
-export default function decorate(block) {
-    console.log(block);
-}
+// export default function decorate(block) {
+//     console.log(block);
+// }
---
+++ blocks/poc-button/poc-button.js
@@ -0,0 +1,6 @@
+
+export default function decorate(block) {
+  console.log("::::", block);
+  block.classList.add('poc-button-decorated');
+  
+}
