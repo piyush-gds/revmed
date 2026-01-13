8dbef314cd177e8aabb171d075eb62e4a8901723
IN010166067_EYGS <Piyush.Jain17@gds.ey.com>
2026-01-13 22:38:20 +0530

    hero banner

--- blocks/revmed-hero-banner/revmed-hero-banner.js
+++ blocks/revmed-hero-banner/revmed-hero-banner.js
@@ -1,31 +1,69 @@
 export default function decorate(block) {
   const rows = Array.from(block.children);
-  const picture = block.querySelector("picture");
-  const img = picture
-    ? picture.querySelector("img")
-    : block.querySelector("img");
 
-  let textContent = null;
-  rows.forEach((row) => {
-    const paragraph = row.querySelector("h1");
-    if (paragraph && paragraph.textContent.trim()) {
-      textContent = paragraph;
-    }
-  });
+  const variation =
+    rows[0]?.querySelector("div")?.textContent.trim() || "image";
+  const assetPicked =
+    rows[1]?.querySelector("picture") ||
+    rows[1]?.querySelector("video") ||
+    rows[1]?.querySelector("a");
+  const text = rows[2]?.querySelector("div")?.innerHTML || "";
 
   block.innerHTML = "";
 
-  if (img) {
-    const imageContainer = document.createElement("div");
-    imageContainer.className = "hero-image";
-    imageContainer.appendChild(img);
-    block.appendChild(imageContainer);
-  }
+  if (variation === "video") {
+    const videoWrapper = document.createElement("div");
+    videoWrapper.className = "hero-video-wrapper";
 
-  if (textContent) {
-    const textContainer = document.createElement("div");
-    textContainer.className = "hero-text";
-    textContainer.appendChild(textContent);
-    block.appendChild(textContainer);
+    const videoContainer = document.createElement("div");
+    videoContainer.className = "hero-video-container";
+
+    const video = document.createElement("video");
+    // const videoSrc = rows[1]?.querySelector('a')?.href;
+    // test video
+    const videoSrc =
+      "https://revmedclinicaltrials.com/videos/hero-video.webm" ||
+      "https://www.w3schools.com/html/mov_bbb.mp4";
+
+    if (videoSrc) {
+      video.src = videoSrc;
+      video.controls = false;
+      video.autoplay = true;
+      video.loop = true;
+      video.muted = true;
+    }
+
+    videoContainer.appendChild(video);
+
+    if (text) {
+      const textContainer = document.createElement("div");
+      textContainer.className = "hero-text-container";
+      textContainer.innerHTML = text;
+      videoContainer.appendChild(textContainer);
+    }
+
+    videoWrapper.appendChild(videoContainer);
+    block.appendChild(videoWrapper);
+  } else if (variation === "image") {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "hero-image-wrapper";
+
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "hero-image-container";
+
+    const img = assetPicked?.querySelector("img") || assetPicked;
+    if (img) {
+      imageContainer.appendChild(img);
+    }
+
+    if (text) {
+      const textContainer = document.createElement("div");
+      textContainer.className = "hero-text-container";
+      textContainer.innerHTML = text;
+      imageContainer.appendChild(textContainer);
+    }
+
+    imageWrapper.appendChild(imageContainer);
+    block.appendChild(imageWrapper);
   }
 }
