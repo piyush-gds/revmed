2618f3d39249e75cff2e9fcc657460d41954efb8
IN010166067_EYGS <Piyush.Jain17@gds.ey.com>
2026-01-15 02:04:03 +0530

    update css

--- blocks/revmed-hero-banner/revmed-hero-banner.js
+++ blocks/revmed-hero-banner/revmed-hero-banner.js
@@ -90,8 +90,12 @@ export default function decorate(block) {
 
     if (text) {
       const textContainer = document.createElement("div");
+      const scrollButton = document.createElement("button");
+      scrollButton.className = "hero-scroll-button";
       textContainer.className = "hero-text-container";
       textContainer.innerHTML = text;
+      scrollButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down text-primary size-8"><path d="M12 5v14"></path><path d="m19 12-7 7-7-7"></path></svg>`;
+      textContainer.appendChild(scrollButton);
       videoContainer.appendChild(textContainer);
     }
 
