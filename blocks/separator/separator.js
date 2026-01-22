ad000f4a1e1c837658deab40b443326bee4e7207
piyush-gds <194486588+piyush-gds@users.noreply.github.com>
2026-01-22 16:52:32 +0530

    change css

--- blocks/separator/separator.js
+++ blocks/separator/separator.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
   const accentBar = document.createElement("div");
-  accentBar.className = "cards-accent-bar";
+  accentBar.className = "separator-accent-bar";
 
   const accentFill = document.createElement("div");
-  accentFill.className = "cards-accent-fill";
+  accentFill.className = "separator-accent-fill";
 
   accentBar.appendChild(accentFill);
   block.appendChild(accentBar);
