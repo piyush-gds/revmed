6942d9afca8c1c81023ce6152975d96cd98caf30
piyush-gds <194486588+piyush-gds@users.noreply.github.com>
2026-01-22 15:40:32 +0530

    Refactor card layout and styling

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,18 +2,52 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
+    
+    const cardHeader = document.createElement('div');
+    cardHeader.className = 'cards-card-header';
+    
+    const cardContent = document.createElement('div');
+    cardContent.className = 'cards-card-content';
+    
+    const cardFooter = document.createElement('div');
+    cardFooter.className = 'cards-card-footer';
+    
+    const accentBar = document.createElement('div');
+    accentBar.className = 'cards-accent-bar';
+    const accentFill = document.createElement('div');
+    accentFill.className = 'cards-accent-fill';
+    accentBar.appendChild(accentFill);
+    
+    while (row.firstElementChild) {
+      const div = row.firstElementChild;
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        cardFooter.append(div);
+        div.className = 'cards-card-image';
+      } else {
+        if (cardHeader.children.length === 0) {
+          cardHeader.append(div);
+          div.className = 'cards-card-title';
+        } else {
+          cardContent.append(div);
+          div.className = 'cards-card-body';
+        }
+      }
+    }
+    
+    li.append(cardHeader);
+    if (cardContent.children.length > 0) {
+      cardContent.insertBefore(accentBar, cardContent.firstChild);
+    }
+    li.append(cardContent);
+    li.append(cardFooter);
+    
     ul.append(li);
   });
+  
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
